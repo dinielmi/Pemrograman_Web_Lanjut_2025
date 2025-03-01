ab6cd16e4276b7fa6ca27321816bf09ff02e5ebc
= <=>
2025-03-01 22:11:51 +0700

    query builder

--- Minggu 3 (PWL_POS)/routes/web.php
+++ Minggu 3 (PWL_POS)/routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\HomepageController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,19 +21,5 @@ Route::get('/', function () {
 
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
 
-// Route::get('/', [HomepageController::class, 'index']);
-
-// Route::prefix('product')->group(function() {
-//     Route::get('/', [ProductController::class, 'index'])->name('product.index');
-//     Route::get('/category/food-beverage', [ProductController::class, 'foodBeverage'])->name('product.food-beverage');
-//     Route::get('/category/beauty-health', [ProductController::class, 'beautyHealth'])->name('product.beauty-health');
-//     Route::get('/category/home-care', [ProductController::class, 'homeCare'])->name('product.home-care');
-//     Route::get('/category/baby-kid', [ProductController::class, 'babyKids'])->name('product.baby-kid');
-// });
-
-// Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
-
-// Route::get('/sales', function(){
-//     return view('transaction');
-// });
