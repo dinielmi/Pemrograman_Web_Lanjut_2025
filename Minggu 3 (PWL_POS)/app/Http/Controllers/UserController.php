35af33e116c7c7f8ceeecc2ed645254552fffb4d
= <=>
2025-03-01 23:12:18 +0700

    Eloquent form

--- Minggu 3 (PWL_POS)/app/Http/Controllers/UserController.php
+++ Minggu 3 (PWL_POS)/app/Http/Controllers/UserController.php
@@ -2,14 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function show($id, $name) {
-        return view('user')
-            ->with('id', $id)
-            ->with('name', $name);
+    public function index() {
+        //tambah data
+        // $data = [
+        // 'username' => 'customer-1',
+        // 'nama' => 'Pelanggan',
+        // 'password' => Hash::make('12345'),
+        // 'level_id' => 4
+        // ];
+
+        //tambah data dengan Eloquent Model
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+
+        // UserModel::insert($data);   //tambahkan data ke table m_user
+        UserModel::where('username', 'customer-1')->update($data);  //update data user
+
+
+        // // Ambil semua data dari tabel m_user (sesuai model UserModel)
+        $user = UserModel::all();
+
+        // Kirim data ke view user.blade.php
+        return view('user', ['data' => $user]);
     }
 }
 
--- Minggu 3 (PWL_POS)/resources/views/user.blade.php
+++ Minggu 3 (PWL_POS)/resources/views/user.blade.php
@@ -1,10 +1,25 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>User</title>
-    </head>
-    <body>
-        <h1>User Information</h1>
-        <h2>Name : {{ $name }}</h2>
-        <h2>ID : {{ $id }}</h2>
-    </body>
+<head>
+    <title>Data User</title>
+</head>
+<body>
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Nama</th>
+            <th>ID Level Pengguna</th>
+        </tr>
+        @foreach ($data as $d)
+        <tr>
+            <td>{{ $d->user_id }}</td>
+            <td>{{ $d->username }}</td>
+            <td>{{ $d->nama }}</td>
+            <td>{{ $d->level_id }}</td>
+        </tr>
+        @endforeach
+    </table>
+</body>
 </html>
