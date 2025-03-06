c50d5a5b5187d63d1b0ac1a5ad84753d6560ba46
= <=>
2025-03-06 20:20:54 +0700

    Retreiving Single Models

--- Minggu 4 (PWL_POS)/app/Http/Controllers/UserController.php
+++ Minggu 4 (PWL_POS)/app/Http/Controllers/UserController.php
@@ -9,39 +9,12 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        // //tambah data
-        // // $data = [
-        // // 'username' => 'customer-1',
-        // // 'nama' => 'Pelanggan',
-        // // 'password' => Hash::make('12345'),
-        // // 'level_id' => 4
-        // // ];
-
-        // // //tambah data dengan Eloquent Model
-        // $data = [
-        //     'nama' => 'Pelanggan Pertama',
-        // ];
-
-        // // UserModel::insert($data);   //tambahkan data ke table m_user
-        // UserModel::where('username', 'customer-1')->update($data);  //update data user
-
-
-        // // // Ambil semua data dari tabel m_user (sesuai model UserModel)
-        // $user = UserModel::all();
-
-        // // Kirim data ke view user.blade.php
-        // return view('user', ['data' => $user]);
-
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-
-        $user = UserModel::all();
+        
+        $user = UserModel::findOr(20, ['username', 'nama'], function (){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
+
     }
 }
 
