a3a9a44717eeb93d34403dbb29f3e7d352c6ff94
= <=>
2025-03-07 13:43:23 +0700

    Not Found Exceptions

--- Minggu 4 (PWL_POS)/app/Http/Controllers/UserController.php
+++ Minggu 4 (PWL_POS)/app/Http/Controllers/UserController.php
@@ -8,13 +8,14 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index() {
-        
-        $user = UserModel::findOr(20, ['username', 'nama'], function (){
-            abort(404);
-        });
-        return view('user', ['data' => $user]);
+    // public function index() {
+    //     $user = UserModel::findOrFail(1);
+    //     return view('user', ['data'=> $user]);
+    // }
 
+    public function index() {
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        return view('user', ['data' => $user]);
     }
 }
 
