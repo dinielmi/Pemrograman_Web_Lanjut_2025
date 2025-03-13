984a8f43ae6009fb363f3cb5e55c5d3d050e51d3
= <=>
2025-03-13 13:08:28 +0700

    Laravel Integration with AdminLte3

--- Minggu 5/routes/web.php
+++ Minggu 5/routes/web.php
@@ -33,3 +33,7 @@ Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
