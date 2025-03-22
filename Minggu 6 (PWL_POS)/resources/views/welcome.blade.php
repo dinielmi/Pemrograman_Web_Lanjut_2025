a0ee0ec6ed19e23847230d7f7b096c43383b7df3
= <=>
2025-03-22 12:16:10 +0700

    first commit

--- Minggu 6 (PWL_POS)/resources/views/welcome.blade.php
+++ Minggu 6 (PWL_POS)/resources/views/welcome.blade.php
@@ -1,27 +1,22 @@
-@extends('layouts.app')
+@extends('layouts.app')  {{--memanggil folder layouts,file app --}}
 
-{{-- Customize layout sections --}}
+{{-- customize layout sections --}}
+@section('subtitle', 'Welcome') {{-- Menetapkan subtitle halaman menjadi "Welcome" --}}
+@section('content_header_title', 'Home') {{-- Menetapkan judul header halaman menjadi "Home" --}}
+@section('content_header_subtitle', 'Welcome') {{-- Menetapkan subtitle header halaman menjadi "Welcome" --}}
 
-@section('subtitle', 'Welcome')
-
-@section('content_header_title', 'Home')
-@section('content_header_subtitle', 'Welcome')
-
-{{-- Content body: main page content --}}
-
+{{-- content body: main page content --}}
 @section('content_body')
-    <p>Welcome to this beautiful admin panel.</p>
-@stop
+    <p>Welcome to this beautiful admin panel</p> {{-- Menampilkan teks sambutan pada panel admin --}}
+@endsection
 
-{{-- Push extra CSS --}}
-
+{{-- push extra css --}}
 @push('css')
-    {{-- Add here extra stylesheets --}}
+    {{-- add here extra stylesheet --}}
     {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
 @endpush
 
-{{-- Push extra scripts --}}
-
+{{-- push extra scripts --}}
 @push('js')
-    <script>console.log("Hi, I'm using the Laravel-AdminLTE package!");</script>
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!");</script>   {{-- Menampilkan pesan di console browser untuk debugging --}}
 @endpush
