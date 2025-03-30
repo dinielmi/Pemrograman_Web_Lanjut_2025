5b397b4e72dd225fe5dd8d6b940711e3e8705adc
= <=>
2025-03-30 17:35:24 +0700

    prak 1 - tambah data user with ajax

--- Minggu 6 (PWL_POS)/resources/views/user/index.blade.php
+++ Minggu 6 (PWL_POS)/resources/views/user/index.blade.php
@@ -6,6 +6,7 @@
        <h3 class="card-title">{{ $page->title }}</h3>
        <div class="card-tools">
            <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a>
+           <button onclick="modalAction('{{ url('user/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
        </div>
    </div>
    <div class="card-body">
@@ -44,6 +45,9 @@
        </table>
    </div>
 </div> 
+<div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" 
+     data-backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true">
+</div>
 @endsection
 
 @push('css') 
@@ -51,62 +55,54 @@
 
 @push('js')
 <script>
-   $(document).ready(function() {
-       var dataUser = $('#table_user').DataTable({
-           // serverSide: true, jika ingin menggunakan server side processing
-           // serverSide: true,
-           ajax: {
-               "url": "{{ url('user/list') }}", 
-               "dataType": "json",
-               "type": "POST",
-               "data": function(d) {
-                        d.level_id = $('#level_id').val();
-                      }
-           },
-           columns: [
-               {
-                   // nomor urut dari laravel datatable addIndexColumn()
-                   data: "DT_RowIndex",
-                   className: "text-center", 
-                   orderable: false, 
-                   searchable: false
-               },
-               {
-                   data: "username", 
-                   className: "",
-                   // orderable: true, jika ingin kolom ini bisa diurutkan
-                   orderable: true,
-                   // searchable: true, jika ingin kolom ini bisa dicari
-                   searchable: true
-               },
-               {
-                   data: "nama",
-                   className: "", 
-                   orderable: true, 
-                   searchable: true
-               },
-               {
-                   // mengambil data level hasil dari ORM berelasi
-                   data: "level.level_nama",
-                   className: "", 
-                   orderable: false, 
-                   searchable: false
-               },
-               {
-                   data: "aksi",
-                   className: "", 
-                   orderable: false, 
-                   searchable: false
-               }
-           ]
-       });
-
-       $('#level_id').on('change', function() {
+       function modalAction(url = '') {
+             $('#myModal').load(url, function() {
+                 $('#myModal').modal('show');
+             });
+         }
+    $(document).ready(function() {
+        var dataUser = $('#table_user').DataTable({
+            serverSide: true,
+            ajax: {
+                url: "{{ url('user/list') }}",
+                dataType: "json",
+                type: "POST",
+                "data": function (d) {
+                    d.level_id = $('#level_id').val();
+                }
+            },
+            columns: [
+                {
+                    data: "DT_RowIndex",
+                    className: "text-center",
+                    orderable: false,
+                    searchable: false
+                },
+                {
+                    data: "username",
+                    orderable: true,
+                    searchable: true
+                },
+                {
+                    data: "nama",
+                    orderable: true,
+                    searchable: true
+                },
+                {
+                    data: "level.level_nama",
+                    orderable: false,
+                    searchable: false
+                },
+                {
+                    data: "aksi",
+                    orderable: false,
+                    searchable: false
+                }
+            ]
+        });
+        $('#level_id').on('change', function() {
             dataUser.ajax.reload();
         });
-
-        
-
-   });
-</script> 
+    });
+</script>
 @endpush
