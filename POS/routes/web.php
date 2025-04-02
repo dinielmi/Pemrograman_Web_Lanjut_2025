396119233a1f1b067b4ac7ce684e187d18a045e7
= <=>
2025-04-02 16:20:15 +0700

    layout & datatables for level

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -52,13 +52,24 @@ Route::get('/user', [UserController::class, 'index']);
 // Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 
 Route::group(['prefix' => 'user'], function () {
-    Route::get('/', [UserController::class, 'index']);         // menampilkan halaman awal user
-    Route::post('/list', [UserController::class, 'list']);      // menampilkan data user dalam bentuk json untuk datatables
-    Route::get('/create', [UserController::class, 'create']);    // menampilkan halaman form tambah user
-    Route::post('/', [UserController::class, 'store']);        // menyimpan data user baru
-    Route::get('/{id}', [UserController::class, 'show']);        // menampilkan detail user
-    Route::get('/{id}/edit', [UserController::class, 'edit']);    // menampilkan halaman form edit user
-    Route::put('/{id}', [UserController::class, 'update']);      // menyimpan perubahan data user
-    Route::delete('/{id}', [UserController::class, 'destroy']);  // menghapus data user
+    Route::get('/', [UserController::class, 'index']);         
+    Route::post('/list', [UserController::class, 'list']);      
+    Route::get('/create', [UserController::class, 'create']);    
+    Route::post('/', [UserController::class, 'store']);       
+    Route::get('/{id}', [UserController::class, 'show']);        
+    Route::get('/{id}/edit', [UserController::class, 'edit']);   
+    Route::put('/{id}', [UserController::class, 'update']);     
+    Route::delete('/{id}', [UserController::class, 'destroy']);  
     Route::post('/data', [UserController::class, 'getUsers'])->name('user.data');
+});
+
+Route::group(['prefix' => 'level'], function () {
+    Route::get('/',[LevelController::class, 'index']);
+    Route::post('/list',[LevelController::class, 'list']);
+    Route::get('/create',[LevelController::class, 'create']);
+    Route::post('/',[LevelController::class, 'store']);
+    Route::get('/{id}',[LevelController::class, 'show']);
+    Route::get('/{id}/edit',[LevelController::class, 'edit']);
+    Route::put('/{id}',[LevelController::class, 'update']);
+    Route::delete('/{id}',[LevelController::class, 'destroy']);
 });
