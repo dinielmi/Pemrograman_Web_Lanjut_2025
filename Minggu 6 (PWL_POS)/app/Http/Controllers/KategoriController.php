51155c7dc9daa690a278d86a0f573edd26d39f5a
= <=>
2025-04-02 13:29:32 +0700

    ajax usage in m_kategori

--- Minggu 6 (PWL_POS)/app/Http/Controllers/KategoriController.php
+++ Minggu 6 (PWL_POS)/app/Http/Controllers/KategoriController.php
@@ -54,12 +54,9 @@ class KategoriController extends Controller
                 // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
                 ->addIndexColumn()
                 ->addColumn('aksi', function ($kategori) { // menambahkan kolom aksi
-                    $btn = '<button onclick="modalAction(\'' . url('/kategori/' . $kategori->kategori_id .
-                        '/show_ajax') . '\')" class="btn btn-info btn-sm">Detail</button> ';
-                    $btn .= '<button onclick="modalAction(\'' . url('/kategori/' . $kategori->kategori_id .
-                        '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> ';
-                    $btn .= '<button onclick="modalAction(\'' . url('/kategori/' . $kategori->kategori_id .
-                        '/delete_ajax') . '\')" class="btn btn-danger btn-sm">Hapus</button> ';
+                    $btn = '<button onclick="modalAction(\'' . url('/kategori/' . $kategori->kategori_id .'/show_ajax') . '\')" class="btn btn-info btn-sm">Detail</button> ';
+                    $btn .= '<button onclick="modalAction(\'' . url('/kategori/' . $kategori->kategori_id .'/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> ';
+                    $btn .= '<button onclick="modalAction(\'' . url('/kategori/' . $kategori->kategori_id .'/delete_ajax') . '\')" class="btn btn-danger btn-sm">Hapus</button> ';
                     return $btn;
                 })
                 ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html
