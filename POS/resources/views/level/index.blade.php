1f800bcba34b6dc509f19405986e9712821b442a
= <=>
2025-04-08 11:21:17 +0700

    layout & datatables for level

--- POS/resources/views/level/index.blade.php
+++ POS/resources/views/level/index.blade.php
@@ -15,7 +15,7 @@
          @if (session('error'))
              <div class="alert alert-danger">{{ session('error') }}</div>
          @endif
-         <table class="table table-bordered table-hover table-sm" id="table_level">
+         <table class="table table-bordered table-hover table-sm table-striped" id="table_level">
              <thead>
                  <tr>
                      <th>ID</th>
