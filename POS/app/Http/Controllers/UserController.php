191f1a84bc72dd7b7e026d830660fd5434a3357d
= <=>
2025-04-08 14:37:08 +0700

    create ajax for user

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -190,5 +191,124 @@ class UserController extends Controller
         ]);
     }
 
+
+    public function create_ajax() {
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+
+        return view('user.create_ajax')
+            -> with('level', $level);
+    }
+
+    public function store_ajax(Request $request)
+    {
+        // cek apakah request berupa ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'level_id' => 'required|integer',
+                'username' => 'required|string|min:3|unique:m_user,username',
+                'nama'     => 'required|string|max:100',
+                'password' => 'required|min:6',
+            ];
+
+            // use Illuminate\Support\Facades\Validator;
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false, // response status, false: error/gagal, true: berhasil
+                    'message' => 'Validasi Gagal',
+                    'msgField' => $validator->errors(), // pesan error validasi
+                ]);
+            }
+
+            UserModel::create($request->all());
+            return response()->json([
+                'status' => true,
+                'message' => 'Data user berhasil disimpan',
+            ]);
+        }
+        return redirect('/');
+    }
+
+    // Menampilkan halaman form edit user ajax
+    public function edit_ajax(string $id)
+    {
+        $user = UserModel::find($id);
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+        
+        return view('user.edit_ajax',['user' => $user, 'level' => $level]);
+    }
+
+    public function update_ajax(Request $request, $id) {
+        // cek apakah request dari ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'level_id' => 'required|integer',
+                'username' => 'required|max:20|unique:m_user,username,'.$id.',user_id',
+                'nama'     => 'required|max:100',
+                'password' => 'nullable|min:6|max:20'
+            ];
+    
+            // use Illuminate\Support\Facades\Validator;
+            $validator = Validator::make($request->all(), $rules);
+    
+            if ($validator->fails()) {
+                return response()->json([
+                    'status'    => false,    // respon json, true: berhasil, false: gagal
+                    'message'   => 'Validasi gagal.',
+                    'msgField'  => $validator->errors() // menunjukkan field mana yang error
+                ]);
+            }
+    
+            $check = UserModel::find($id);
+            if ($check) {
+                // jika password tidak diisi, maka hapus dari request
+                if (!$request->filled('password')) {
+                    $request->request->remove('password');
+                }
+    
+                $check->update($request->all());
+                return response()->json([
+                    'status'    => true,
+                    'message'   => 'Data berhasil diupdate'
+                ]);
+            } else {
+                return response()->json([
+                    'status'    => false,
+                    'message'   => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+    
+        return redirect('/');
+    }
+
+    public function confirm_ajax(string $id) {
+        $user = UserModel::find($id);
+    
+        return view('user.confirm_ajax', ['user' => $user]);
+    }    
+
+    public function delete_ajax(Request $request, $id)
+    {
+        // cek apakah request dari ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $user = UserModel::find($id);
+            if ($user) {
+                $user->delete();
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil dihapus'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+
+        return redirect('/');
+    }
 
 }
--- POS/routes/web.php
+++ POS/routes/web.php
@@ -60,12 +60,22 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/', [UserController::class, 'index']);         
     Route::post('/list', [UserController::class, 'list']);      
     Route::get('/create', [UserController::class, 'create']);    
-    Route::post('/', [UserController::class, 'store']);       
+    Route::post('/', [UserController::class, 'store']);  
+    
+        //route ajax
+        Route::get('/create_ajax', [UserController::class, 'create_ajax']);  
+        Route::post('/ajax', [UserController::class, 'store_ajax']);        
+        Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);        
+        Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);    
+        Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']); 
+        Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']);
+
     Route::get('/{id}', [UserController::class, 'show']);        
     Route::get('/{id}/edit', [UserController::class, 'edit']);   
     Route::put('/{id}', [UserController::class, 'update']);     
     Route::delete('/{id}', [UserController::class, 'destroy']);  
     Route::post('/data', [UserController::class, 'getUsers'])->name('user.data');
+
 });
 
 Route::group(['prefix' => 'level'], function () {
