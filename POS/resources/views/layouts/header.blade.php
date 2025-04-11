1f5e6ad345665c4ecbd5e29e3fc1f59d04e88b1c
= <=>
2025-04-11 19:56:04 +0700

    add logout function

--- POS/resources/views/layouts/header.blade.php
+++ POS/resources/views/layouts/header.blade.php
@@ -140,7 +140,7 @@
             <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                 <i class="fas fa-sign-out-alt"></i> Logout <!-- Ikon logout -->
             </a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            <form id="logout-form" action="{{ url('logout') }}" method="POST" style="display: none;">
                 @csrf
             </form>
         </div>
