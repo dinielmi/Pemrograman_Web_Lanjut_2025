d0e108f8f8a42ba8acc57c41d098d63cc3a56ac4
= <=>
2025-04-12 17:47:06 +0700

    add export function

--- Minggu 8 (PWL_POS)/routes/web.php
+++ Minggu 8 (PWL_POS)/routes/web.php
@@ -154,6 +154,7 @@ Route::middleware(['authorize:ADMIN, STF'])->group(function () {
         Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
         Route::get('/import', [BarangController::class, 'import']); // ajax form upload excel
         Route::post('/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
+        Route::get('/export_excel', [BarangController::class, 'export_excel']); // export excel
 
         Route::get('/{id}',[BarangController::class, 'show']);
         Route::get('/{id}/edit',[BarangController::class, 'edit']);
