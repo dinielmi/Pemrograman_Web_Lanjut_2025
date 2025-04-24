3a02cfa8af6ea1620b4c70cdaac3ed34b70a69a9
= <=>
2025-04-24 13:43:51 +0700

    tugas implementasi eloquent accessor untuk m_barang

--- Minggu 11 (PWL_POS)/app/Models/BarangModel.php
+++ Minggu 11 (PWL_POS)/app/Models/BarangModel.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class BarangModel extends Model
 {
@@ -16,10 +17,25 @@ class BarangModel extends Model
      *
      * @var array
      */
-    protected $fillable = ['kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual'];
+    protected $fillable = [
+        'barang_kode',
+        'barang_nama',
+        'kategori_id',
+        'harga_beli',
+        'harga_jual',
+        'image' // tambah ini
+    ];
 
     public function kategori(): BelongsTo 
     {
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
     }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => $image ? url('/storage/barang/' . $image) : null
+        );
+    }
+
 }
